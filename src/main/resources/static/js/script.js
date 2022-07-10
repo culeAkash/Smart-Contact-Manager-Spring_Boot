511fe3b47c1fd353a9cfd8dba75da9851f7953c7
Akash <akashjais929@gmail.com>
2022-07-10 20:32:53 +0530

    Added Responsive Sidebar to User Dashboard
    Used pure CSS and JavaScript to add responsive sidebar and buttons

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,15 @@
+
+//when cross or bar is pressed if the sidebar is visible then hide it and vice versa
+const toggleSidebar= ()=>{
+    if($('.sidebar').is(":visible")){
+        //true then close side bar
+        $('.sidebar').css("display","none");
+        $(".content").css("margin-left","0%");
+    }
+    else{
+        //if not visible make sidebar visible
+        $('.sidebar').css("display","block");
+        $(".content").css("margin-left","20%");
+    }
+};
+
