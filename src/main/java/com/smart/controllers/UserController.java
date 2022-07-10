0b5911fe009ffe256303562b9ea4499ab2cdb413
Akash <akashjais929@gmail.com>
2022-07-10 19:32:36 +0530

    Added User Dashboard
    Now add Sidebar

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -1,8 +1,15 @@
 package com.smart.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user") // all the handlers in this controller now, will start with /user and then the
@@ -10,8 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //this is done to authenticate all the urls with user
 public class UserController {
 
+	@Autowired
+	UserRepository repo;
+
 	@GetMapping("/index") // url for this => /user/index
-	public String dashboard() {
+	public String dashboard(Model model, Principal principal) {
+		String userName = principal.getName();// email of the user will be returned
+
+		// get details from database using email
+		User loggedInUser = this.repo.getUserByUserName(userName);
+		System.out.println(loggedInUser);
+
+		// Sending user data to view
+		model.addAttribute("user", loggedInUser);
+		model.addAttribute("title", "Dashboard - " + loggedInUser.getName());
+
 		return "normal/user_dashboard";
 	}
+	// Principal => it is Spring Security class which helps to get different details
+	// about the logged in user
 }
